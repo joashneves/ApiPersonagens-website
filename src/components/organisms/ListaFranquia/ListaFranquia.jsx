3c79d2788ba8b8902341f2791783e745b9e1e77d
joashneves <joashneves@gmail.com>
2025-03-24 14:27:30 -0300

    muda algo

--- src/components/organisms/ListaFranquia/ListaFranquia.jsx
+++ src/components/organisms/ListaFranquia/ListaFranquia.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import FranquiaTemplate from '../../atoms/franquiaTemplate/FranquiaTemplate';
 import ProcurarFranquia from '../../atoms/procurarFranquia/ProcurarFranquia';
